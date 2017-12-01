c89b2793af4dfa5d5b0f29a27531311206cbbca7
Francesco Casula <fracasula@users.noreply.github.com>
2017-12-01 14:07:22 +0000

    Test suite now only runs *Spec.js files

--- app/test/index.js
+++ app/test/index.js
@@ -3,5 +3,5 @@ import Vue from 'vue';
 Vue.config.productionTip = false;
 
 // require all test files (files that ends with .js)
-const testsContext = require.context('./specs', true, /\.js/);
+const testsContext = require.context('./specs', true, /Spec\.js$/);
 testsContext.keys().forEach(testsContext);
